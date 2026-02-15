5a5f29d790c4f84122da62f1a4497bc66bbfeecb
elson <elsonnezaj2005@gmail.com>
2026-02-15 11:36:31 +0100

    created first time featured base structure for project

---
+++ src/components/Content/index.tsx
@@ -0,0 +1,8 @@
+import { type ReactElement } from "react";
+import styles from "./styles.module.scss";
+
+export default function Content(): ReactElement {
+  return (
+    <div className={styles.content}></div>
+  )
+}
