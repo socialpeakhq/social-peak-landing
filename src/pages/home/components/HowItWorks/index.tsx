fc729b8dd41e1952262b5bdaa093f24fd0e6ed47
elson <elsonnezaj2005@gmail.com>
2026-02-18 09:46:17 +0100

    fixes for card version 1

--- src/pages/home/components/HowItWorks/index.tsx
+++ src/pages/home/components/HowItWorks/index.tsx
@@ -1,11 +1,18 @@
 import { type ReactElement } from "react";
 import { Box, Typography } from "@mui/material";
+import Card from "../../../../components/Card/Container";
+import { HOW_IT_WORKS_CARDS, type CardProps } from "../../../../utilities/constants/homeConsts";
 import styles from "./styles.module.scss"
 
 export default function HowItWorks(): ReactElement {
   return (
     <Box className={styles.howItWorksContainer}>
       <Typography className={styles.sectionTitle}>How Social Peak Works</Typography>
+      <Box className={styles.cardsContainer}>
+        {HOW_IT_WORKS_CARDS.map((item: CardProps, index: number) =>
+          <Card key={index} card={item} type="version2" />
+        )}
+      </Box>
     </Box>
   )
 }
