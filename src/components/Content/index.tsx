161383089c09f1907321ea38450a22b5ee0327c4
elson <elsonnezaj2005@gmail.com>
2026-02-28 12:19:58 +0100

    started working on how it works page

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -5,6 +5,7 @@ import { Route, Routes } from "react-router";
 const Home = lazy(() => import("../../pages/home/Container"))
 const Features = lazy(() => import("../../pages/features/Container"))
 const Pricing = lazy(() => import("../../pages/pricing/Container"))
+const HowItWorks = lazy(() => import("../../pages/howItWorks/Container"))
 
 export default function Content(): ReactElement {
   return (
@@ -14,6 +15,7 @@ export default function Content(): ReactElement {
         <Route path="/home" element={<Home />} />
         <Route path="/features" element={<Features />} />
         <Route path="/pricing" element={<Pricing />} />
+        <Route path="how-it-works" element={<HowItWorks />} />
       </Routes>
     </div>
   )
---
+++ src/pages/howItWorks/Container/index.tsx
@@ -0,0 +1,12 @@
+import { type ReactElement } from "react";
+import { Box, Typography } from "@mui/material";
+import styles from "./styles.module.scss"
+
+export default function HowItWorks(): ReactElement {
+  return (
+    <Box className={styles.container}>
+      <Typography className={styles.title}>How Social Peak Works</Typography>
+      <Typography className={styles.pageDescription}>Streamline your social media management with our unified platform. Connect your Instagram Business and Facebook Pages, sync analytics, and manage everything from one centralized dashboard.</Typography>
+    </Box>
+  )
+}
