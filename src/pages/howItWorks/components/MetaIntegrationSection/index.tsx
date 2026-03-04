3969bbf6522dfd4595968efee80d51193df14a6c
elson <elsonnezaj2005@gmail.com>
2026-03-04 19:09:15 +0100

    creating new section

---
+++ src/pages/howItWorks/components/MetaIntegrationSection/index.tsx
@@ -0,0 +1,9 @@
+import { type ReactElement } from "react";
+import styles from "./styles.module.scss"
+import { Box } from "@mui/material";
+
+export default function MetaIntegrationSection(): ReactElement {
+  return (
+    <Box className={styles.metaIntegrationContainer}></Box>
+  )
+}
